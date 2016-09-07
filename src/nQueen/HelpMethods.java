84bc387ddd04fcfae8625404b1b8ceb706fb03ec
MaSommer <martisom@stud.ntnu.no>
2016-09-07 11:29:06 +0200

    SwitchtoqueenRandom

--- src/nQueen/HelpMethods.java
+++ src/nQueen/HelpMethods.java
@@ -1,5 +1,6 @@
 package nQueen;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -30,7 +31,7 @@ public class HelpMethods {
 			}
 		}
 		for(int column = 0 ; column < numberOfRows ; column++) {
-				indexedConflicts[column] -=1;
+			indexedConflicts[column] -=1;
 
 		}
 
@@ -84,86 +85,97 @@ public class HelpMethods {
 		}
 
 		for(int i = 0 ; i < indexedConflicts.length ; i++) {
-				indexedConflicts[i] -= 1 ;
+			indexedConflicts[i] -= 1 ;
 		}
 
 		return indexedConflicts;
 	}
-	
+
 	public static int[] countDiagonalConflictsFast(int[] queenPositions) {
 		int numberOfDiagonals = (queenPositions.length*2-2)*2;
 		int[] indexedConflicts = new int[numberOfDiagonals];
-		
-		for(int i = 0 ; i < queenPositions.length ; i++){ //legger inn konflikter i diagonal opp til h¿yre
+
+		for(int i = 0 ; i < queenPositions.length ; i++){ //legger inn konflikter i diagonal opp til hoyre
 			int rowIndex = queenPositions.length - queenPositions[i] - 1;
 			int columnIndex = i;
-			
+
 			if(rowIndex+columnIndex > 0 && rowIndex + columnIndex < queenPositions.length*2){
 				indexedConflicts[rowIndex+columnIndex]+=1;
 			}
 		}
-		
+
 		int offset = numberOfDiagonals/2;
-		
+
 		for(int i = 0 ; i < queenPositions.length ; i++) {
 			int rowIndex = queenPositions.length - queenPositions[i];
 			//System.out.println(rowIndex +"    min metode");
 			int columnIndex = queenPositions.length - i - 1;
-			
+
 			if(columnIndex + rowIndex > 0 && columnIndex + rowIndex < queenPositions.length*2-2) {
 				indexedConflicts[offset  + rowIndex + columnIndex-1]+=1;
 			}
-			
+
 		}
-		
+
 		for(int i = 0 ; i < indexedConflicts.length ; i++) {
 			indexedConflicts[i] -= 1 ;
 		}
-	
+
 		return indexedConflicts;
-		
+
 	}
-	
+
 	public static Board[] createNeighbours(Board board, int numberOfNeighbours) {
 		Random random = new Random();
 		Board[] neighbours = new Board[numberOfNeighbours];
 		for(int i = 0 ; i < numberOfNeighbours ; i++) {
 			neighbours[i] = new Board(board.getQueenPositions());
-			
+
 			int kolonne = random.nextInt(board.getSize());
 			int offset = random.nextInt(board.getSize());
 			int newRow = (neighbours[i].getQueenPositions()[kolonne] + offset) % board.getSize();
-//			System.out.println("\n\n");
-//			System.out.println();
+			//			System.out.println("\n\n");
+			//			System.out.println();
 			//System.out.println(Arrays.toString(neighbours[i].getQueenPositions()));
 			neighbours[i].moveQueen(kolonne, newRow);
 			//System.out.println(Arrays.toString(neighbours[i].getQueenPositions()));
-//			System.out.println(neighbours[i]);
-//			System.out.println("                             \n\n");
-			
+			//			System.out.println(neighbours[i]);
+			//			System.out.println("                             \n\n");
+
 		}
 		return neighbours;
 
-		
+
 	}
-	
+
 	public static int simpleHeuristic(Board board) {
 		int heuristicValue = 0;
 		int[] conflicts = HelpMethods.countConflicts(board);
 		for(int i = 0 ; i < conflicts.length ; i++){
 			if(conflicts[i] > 0)
-			heuristicValue +=conflicts[i];
-			
+				heuristicValue +=conflicts[i];
+
 		}
-		
+
 		return heuristicValue;
 	}
-	
+
 	public static int generateRandomNumer(int min, int max){
 		Random rand = new Random();
 		return rand.nextInt((max - min) +1)+min;
 
 	}
-	
+
+	public static int[] switchToQueenRandom(int[] list){
+		int pos1 = HelpMethods.generateRandomNumer(0, list.length-1);
+		int pos2 = HelpMethods.generateRandomNumer(0, list.length-1);
+		int save = list[pos1];
+		list[pos1] = list[pos2];
+		list[pos2] = save;
+		return list;
+
+
+	}
+
 
 }
