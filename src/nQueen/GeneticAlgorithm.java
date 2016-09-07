2aea290e302d636b34e97a97847559b0228d9696
MaSommer <martisom@stud.ntnu.no>
2016-09-07 13:55:09 +0200

    GA impoved

--- src/nQueen/GeneticAlgorithm.java
+++ src/nQueen/GeneticAlgorithm.java
@@ -11,67 +11,41 @@ public class GeneticAlgorithm {
 	private ArrayList<int[]> randomSolutions;
 	private ArrayList<int[]> solutions;
 	private HashMap<Integer, Integer> numberOfSolutionsTable;
+	private HashMap<Integer, int[]> ranking;
 	private final double INF = Double.MAX_VALUE;
 	private int numberOfSol;
 	private int size;
+	private int mutationRate;
 
-	public GeneticAlgorithm(Board board){
+	public GeneticAlgorithm(Board board, int mutationRate){
 		randomSolutions = new ArrayList<int[]>();
+		this.mutationRate = mutationRate;
 		solutions = new ArrayList<int[]>();
 		numberOfSolutionsTable = new HashMap<Integer, Integer>();
-		generateNumberOfSolutions();
-		numberOfSol = numberOfSolutionsTable.get(board.getQueenPosition().length);
+		ranking = new HashMap<Integer, int[]>();
+//		numberOfSol = numberOfSolutionsTable.get(board.getQueenPosition().length);
+		numberOfSol = 100;
 		size = board.getQueenPosition().length;
-		while (1 != solutions.size()) {
-			randomSolutions = generateRandomSolutions(numberOfSol);
+		randomSolutions = generateRandomSolutions(numberOfSol);
+		int iterations = 0;
+		while (solutions.size() < 1) {
 			for (int[] sol : randomSolutions) {
-				Board b = new Board(sol);			
+				Board b = new Board(sol);
 				if (b.checkIfLegal()){
 					if (!solutions.contains(sol)){
 						solutions.add(sol.clone());
 					}
 				}
 			}
-//			System.out.println(solutions.size());
-			doFitnessEvaluation();			
+			iterations++;
+			doFitnessEvaluation();	
+			
 		}
 		for (int[] sol : solutions) {
 			System.out.println(Arrays.toString(sol));
 		}
 		System.out.println("Number of solutions for " + size + "x" + size + ": " + solutions.size());
-	}
-
-	public void generateNumberOfSolutions(){
-		numberOfSolutionsTable.put(1, 0);
-		numberOfSolutionsTable.put(2, 0);
-		numberOfSolutionsTable.put(3, 0);
-		numberOfSolutionsTable.put(4, 2);
-		numberOfSolutionsTable.put(5, 10);
-		numberOfSolutionsTable.put(6, 4);
-		numberOfSolutionsTable.put(7, 40);
-		numberOfSolutionsTable.put(8, 92);
-		numberOfSolutionsTable.put(9, 352);
-		numberOfSolutionsTable.put(10, 724);
-		numberOfSolutionsTable.put(11, 724);
-		numberOfSolutionsTable.put(12, 724);
-		numberOfSolutionsTable.put(13, 724);
-		numberOfSolutionsTable.put(14, 724);
-		numberOfSolutionsTable.put(15, 724);
-		numberOfSolutionsTable.put(16, 724);
-		numberOfSolutionsTable.put(17, 724);
-		numberOfSolutionsTable.put(18, 724);
-		numberOfSolutionsTable.put(19, 724);
-		numberOfSolutionsTable.put(20, 724);
-		numberOfSolutionsTable.put(21, 724);
-		numberOfSolutionsTable.put(22, 724);
-		numberOfSolutionsTable.put(23, 724);
-		numberOfSolutionsTable.put(24, 724);
-		numberOfSolutionsTable.put(25, 724);
-		numberOfSolutionsTable.put(26, 724);
-		numberOfSolutionsTable.put(27, 724);
-		numberOfSolutionsTable.put(28, 724);
-		numberOfSolutionsTable.put(29, 724);
-		numberOfSolutionsTable.put(30, 724);
+		System.out.println("Number of iterations: " + iterations);
 	}
 
 	public ArrayList<int[]> generateRandomSolutions(int numberOfSol){
@@ -80,10 +54,6 @@ public class GeneticAlgorithm {
 			int[] randomSol = generateRandomList(1, size);
 			ret.add(randomSol);
 		}
-//		for (int[] sol : ret) {
-//			System.out.println(Arrays.toString(sol));
-//		}
-//		System.out.println(ret.size());
 		return ret;
 	}
 
@@ -123,14 +93,10 @@ public class GeneticAlgorithm {
 
 	public void selection(double[] fitnessEvaluation){
 		int length = 0;
-		if (fitnessEvaluation.length%2 == 1){
-			length = (fitnessEvaluation.length/2)+1;			
-		}
-		else{
-			length = fitnessEvaluation.length/2;						
-		}
 		ArrayList<int[]> newRandomSolution = new ArrayList<int[]>();
-		int veryBest = 0;
+		//Setter length til 20 og velger da ut bare de 20 beste brettene.
+		length = randomSolutions.size()/2;
+		int c = 0;
 		for (int j = 0; j < length; j++) {
 			double best = 1;
 			int bestPosition = 0;
@@ -140,81 +106,87 @@ public class GeneticAlgorithm {
 					bestPosition = j2;
 				}		
 			}
-			if (j == 0){
-				veryBest = bestPosition;
-			}
-//			System.out.println("best: " + best);
-//			System.out.println("bestPos: " + bestPosition);
 			fitnessEvaluation[bestPosition] = INF;
-			newRandomSolution.add(randomSolutions.get(bestPosition));
-			newRandomSolution.add(randomSolutions.get(veryBest));
+			ranking.put(c, randomSolutions.get(bestPosition));
+			c++;
 		}
-//		System.out.println();
-//		for (int[] is : newRandomSolution) {
-//
-//			System.out.println(Arrays.toString(is));
-//		}
-//		System.out.println();
-		doCrossover(newRandomSolution);
+		//Legger til de 50% beste fra randomSolutions
+		for (int i = 0; i < ranking.size(); i++) {
+			newRandomSolution.add(ranking.get(i));
+		}
+		//Legger til de 50% beste fra randomSolutions men denne gang slik: 1st, 20th, 2nd, 19th, 3rd, 18th, ...
+		for (int i = 0; i < ranking.size()/2; i++) {
+			newRandomSolution.add(ranking.get(i));
+			newRandomSolution.add(ranking.get(ranking.size()-1));
+		}
+		//System.out.println(newRandomSolution);
+		doCrossover2(newRandomSolution);
 	}
-
-	public void doCrossover(ArrayList<int[]> newRandomSolution){
+	
+	public void doCrossover2(ArrayList<int[]> newRandomSolution){
+		int a;
+		if (size%2 == 1){
+			a = (size/2+1);
+		}
+		else{
+			a = size/2;
+		}
 		for (int i = 0; i < newRandomSolution.size(); i+=2) {
-			int[] newQueenPos1 = new int[size];
-			int[] newQueenPos2 = new int[size];
-			int r = HelpMethods.generateRandomNumer(1, size);
-			boolean check = true;
-			while (check){
-				r = HelpMethods.generateRandomNumer(1, size);
-				//System.out.println("r: " + r);
-				int[] list1 = new int[r];
-				int[] list2 = new int[r];
-				for (int j = 0; j < r; j++) {
-					list1[j] = newRandomSolution.get(i)[j];
-					list2[j] = newRandomSolution.get(i+1)[j];
-				}
-				Arrays.sort(list1);
-				Arrays.sort(list2);
-				boolean listCheck = true;
-				for (int j = 0; j < r; j++) {
-					if (list1[j] != list2[j]){
-						listCheck = false;
-						break;
+			int[] newQueen1 = newRandomSolution.get(i).clone();
+			int[] newQueen2 = newRandomSolution.get(i+1).clone();
+			int r = HelpMethods.generateRandomNumer(1, a/2)*2;
+			ArrayList<Integer> flipList = new ArrayList<Integer>();
+			ArrayList<Integer> posToFlip = new ArrayList<Integer>();
+			for (int j = 0; j < r; j++) {
+				int positionToFlip = HelpMethods.generateRandomNumer(0, size-1);
+				flipList.add(newQueen1[positionToFlip]);
+				posToFlip.add(positionToFlip);
+			}
+			for (int j = 0; j < r; j+=2) {
+				int queen1ToFlip1 = flipList.get(j);
+				int queen1ToFlip2 = flipList.get(j+1);
+				for (int k = 0; k < newQueen2.length; k++) {
+					if (newQueen1[k] == queen1ToFlip1){
+						newQueen1[k] = queen1ToFlip2;
+						continue;
+					}
+					if (newQueen1[k] == queen1ToFlip2){
+						newQueen1[k] = queen1ToFlip1;
+						continue;
 					}
 				}
-//				System.out.println("list1: "+Arrays.toString(list1));
-//				System.out.println("list2: "+Arrays.toString(list2));
-				if (listCheck){
-					check = false;
+				for (int k = 0; k < newQueen2.length; k++) {
+					if (newQueen2[k] == queen1ToFlip1){
+						newQueen2[k] = queen1ToFlip2;
+						continue;
+					}
+					if (newQueen2[k] == queen1ToFlip2){
+						newQueen2[k] = queen1ToFlip1;
+						continue;
+					}				
 				}
 			}
-//			System.out.println("Random: " + r);
-			for (int j = 0; j < r; j++) {
-				newQueenPos1[j] = newRandomSolution.get(i)[j];
-				newQueenPos2[j] = newRandomSolution.get(i+1)[j];
-			}
-			for (int j = r; j < size; j++) {
-				newQueenPos1[j] = newRandomSolution.get(i+1)[j];
-				newQueenPos2[j] = newRandomSolution.get(i)[j];
-			}
-			newRandomSolution.set(i, newQueenPos1);
-			newRandomSolution.set(i+1, newQueenPos2);
+			newRandomSolution.set(i, newQueen1);
+			newRandomSolution.set(i+1, newQueen2);
 		}
-//		for (int[] is : newRandomSolution) {
-//			System.out.println(Arrays.toString(is));
-//		}
 		doMutation(newRandomSolution);
-
 	}
 
 	public void doMutation(ArrayList<int[]> newRandomSolution){
 		for (int[] is : newRandomSolution) {
-			int c = HelpMethods.generateRandomNumer(1, 6);
-			if (c == 2){//doing mutation with a small independent probability
-				int col = HelpMethods.generateRandomNumer(0, size-1);
-				int newRow = HelpMethods.generateRandomNumer(1, size);
-				is[col] = newRow;
+			Board b = new Board(is);
+			if (b.checkIfLegal()){
+				break;
 			}
+			int a = HelpMethods.generateRandomNumer(1, 100);
+			if (a > mutationRate){//doing mutation with a small independent probability
+				int pos1 = HelpMethods.generateRandomNumer(0, size-1);
+				int pos2 = HelpMethods.generateRandomNumer(0, size-1);
+				int save = is[pos1];
+				is[pos1] = is[pos2];
+				is[pos2] = save;
+			}
+			
 
 		}
 		randomSolutions = newRandomSolution;
